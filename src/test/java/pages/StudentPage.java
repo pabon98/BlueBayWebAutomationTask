354a1f2e9a81bc69acdaf2c19d31839cda9b64f1
pabon98 <hasinanjumpabon@gmail.com>
2025-03-23 17:25:26 +0600

    :delete student method added

--- src/test/java/pages/StudentPage.java
+++ src/test/java/pages/StudentPage.java
@@ -1,7 +1,5 @@
 package pages;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.Select;
 
 public class StudentPage extends BasePage{
     public String student_page_url = "http://test.uapp.uk/studentList";
@@ -16,8 +14,10 @@ public class StudentPage extends BasePage{
     public By create_student_Btn_locator = By.xpath("//button[@type='submit']");
     public By student_add_alert = By.xpath("//b[normalize-space()='Student added successfully']");
     public By student_complete_profile_locator = By.xpath("//button[normalize-space()='Complete Profile']");
-    public String student_page_url = "http://test.uapp.uk/studentList";
-
+    public By confirm_delete_btn_text = By.xpath("//b[normalize-space()='Do You Want To Delete This Student Information ?']");
+    public By delete_student_btn_locator = By.xpath("//tbody/tr[1]/td[10]/div[1]/button[3]");
+    public By confirm_delete_student_btn_locator = By.xpath("//button[@type='submit']");
+    public By delete_student_toast_alert = By.xpath("//div[@id='navbar-mobile']");
 
 
     public void LoadStudentPage(){
--- src/test/java/testcases/TestStudentPage.java
+++ src/test/java/testcases/TestStudentPage.java
@@ -45,7 +45,7 @@ public class TestStudentPage extends DriverSetUp {
         studentPage.writeOneElement(studentPage.last_name_locator, "pabon");
         getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         // Email
-        studentPage.writeOneElement(studentPage.email_locator, "hpabon1234@mail.com");
+        studentPage.writeOneElement(studentPage.email_locator, "hpabon12240@mail.com");
         getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         // Create Student
         studentPage.clickOneElement(studentPage.create_student_Btn_locator);
@@ -54,6 +54,17 @@ public class TestStudentPage extends DriverSetUp {
         alertWait.until(ExpectedConditions.textToBePresentInElementLocated(studentPage.student_add_alert, "Student added successfully"));
         studentPage.clickOneElement(studentPage.student_complete_profile_locator);
 
+    }
+
+    @Test
+    public void DeleteAStudent() throws InterruptedException {
+       studentPage.LoadAnWebPage(studentPage.student_page_url);
+       studentPage.clickOneElement(studentPage.delete_student_btn_locator);
+       // wait until modal show the text
+       WebDriverWait confirmDeleteWait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+       confirmDeleteWait.until(ExpectedConditions.textToBePresentInElementLocated(studentPage.confirm_delete_btn_text, "Do You Want To Delete This Student Information ?"));
+       studentPage.clickOneElement(studentPage.confirm_delete_student_btn_locator);
+       Thread.sleep(3000);
     }
 
 }
