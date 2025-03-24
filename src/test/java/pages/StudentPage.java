54fcbe58d20a05cb9cc4c8e18d227d1450032c11
pabon98 <hasinanjumpabon@gmail.com>
2025-03-24 11:33:25 +0600

    added student updated method

--- src/test/java/pages/StudentPage.java
+++ src/test/java/pages/StudentPage.java
@@ -18,6 +18,27 @@ public class StudentPage extends BasePage{
     public By delete_student_btn_locator = By.xpath("//tbody/tr[1]/td[10]/div[1]/button[3]");
     public By confirm_delete_student_btn_locator = By.xpath("//button[@type='submit']");
     public By delete_student_toast_alert = By.xpath("//div[@id='navbar-mobile']");
+    public By edit_student_locator = By.xpath("//body[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[3]/div[1]/div[2]/table[1]/tbody[1]/tr[1]/td[10]/div[1]/button[2]/i[1]");
+    public By edit_student_locator_title = By.xpath("//p[@class='section-title']");
+    public By edit_student_consultant = By.xpath("//div[@id='consultantId']//div[@class='css-19bb58m']");
+    public By edit_title = By.cssSelector("input[value='1'][name='nameTittleId']");
+    public By edit_firstname = By.xpath("//input[@id='firstName']");
+    public By edit_lastName = By.xpath("//input[@id='lastName']");
+    public By edit_date_of_birth = By.xpath("//input[@id='dateOfBirth']");
+    public By edit_date_picker_body = By.xpath("//div[@class='ant-picker-body']");
+    public By edit_passportNumber = By.xpath("//input[@id='passportNumber']");
+    public By edit_issue_date = By.xpath("//input[@id='issueDate']");
+    public By edit_expire_date = By.xpath("//input[@id='expiresDate']");
+    public By edit_gender = By.cssSelector("input[value='1'][name='genderId']");
+    public By edit_marital_status = By.cssSelector("input[value='1'][name='maritalStatusId']");
+    public By edit_phone_number = By.xpath("//input[@placeholder='1 (702) 123-4567']");
+    public By edit_email = By.xpath("//input[@id='email']");
+    public By edit_country_birth = By.xpath("//div[@id='countryOfBirthId']//div[@class='css-19bb58m']");
+    public By edit_residence_country = By.cssSelector("div[id='countryOfCitizenship'] div[class='css-19bb58m']");
+    public By edit_country_nationality = By.xpath("//div[@id='nationalityId']//div[@class='css-19bb58m']");
+    public By save_and_next =By.xpath("//button[normalize-space()='Save and Next']");
+    public By student_info_updated_toast = By.xpath("//div[@class='content-wrapper coustom-wrapper']");
+    public By go_to_stu_page = By.xpath("//span[@class='text-white']");
 
 
     public void LoadStudentPage(){
--- src/test/java/testcases/TestStudentPage.java
+++ src/test/java/testcases/TestStudentPage.java
@@ -6,8 +6,10 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import pages.StudentPage;
 import utilities.DriverSetUp;
-
 import java.time.Duration;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 
 public class TestStudentPage extends DriverSetUp {
     StudentPage studentPage = new StudentPage();
@@ -45,7 +47,9 @@ public class TestStudentPage extends DriverSetUp {
         studentPage.writeOneElement(studentPage.last_name_locator, "pabon");
         getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         // Email
-        studentPage.writeOneElement(studentPage.email_locator, "hpabon12240@mail.com");
+        // generate random email
+        String randomEmail = "user" + UUID.randomUUID().toString().substring(0,8) +"@mail.com";
+        studentPage.writeOneElement(studentPage.email_locator, randomEmail);
         getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         // Create Student
         studentPage.clickOneElement(studentPage.create_student_Btn_locator);
@@ -56,15 +60,162 @@ public class TestStudentPage extends DriverSetUp {
 
     }
 
+
+
+    @Test
+    public void EditAStudent() throws InterruptedException {
+        studentPage.LoadAnWebPage(studentPage.student_page_url);
+        studentPage.clickOneElement(studentPage.edit_student_locator);
+        WebDriverWait editWait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        editWait.until(ExpectedConditions.textToBePresentInElementLocated(studentPage.edit_student_locator_title, "Personal Information"));
+
+        // Edit consultant
+        studentPage.clickOneElement(studentPage.edit_student_consultant);
+        Actions editConsultantAction = new Actions(getDriver());
+        editConsultantAction.sendKeys("Talha Arif").build().perform();
+        editConsultantAction.sendKeys(Keys.ENTER).build().perform();
+
+        // Edit title
+        studentPage.clickOneElement(studentPage.edit_title);
+        //Edit firstName
+        studentPage.getElement(studentPage.edit_firstname).clear();
+        studentPage.writeOneElement(studentPage.edit_firstname, "Md. Habib");
+        //Edit lastName
+        studentPage.getElement(studentPage.edit_lastName).clear();
+        studentPage.writeOneElement(studentPage.edit_lastName, "Hossain Khan");
+
+        //Edit Date Of Birth
+
+        //step 1 Click date input field to open date picker
+        studentPage.clickOneElement(studentPage.edit_date_of_birth);
+        WebDriverWait datePickerWait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        datePickerWait.until(ExpectedConditions.visibilityOfElementLocated(studentPage.edit_date_picker_body));
+
+        // step 2 click the year selection to open year selection
+        WebElement yearSelector = datePickerWait.until(ExpectedConditions.elementToBeClickable(By.className("ant-picker-year-btn")));
+        yearSelector.click();
+
+        // Step 3: Navigate Back Until We Reach a Year Before 2010
+        while (true) {
+            // Get the first  displayed year from the list
+            List<WebElement> years = getDriver().findElements(By.xpath("//div[contains(@class,'ant-picker-cell-inner')]"));
+            int firstDisplayedYear = Integer.parseInt(years.getFirst().getText());
+
+            // Stop when the first displayed year is before 2010
+            if (firstDisplayedYear <= 2000) {
+                break;
+            }
+
+            // Click the "Previous Decade" button to go back 10 years
+            WebElement prevDecadeButton = getDriver().findElement(By.className("ant-picker-super-prev-icon"));
+            prevDecadeButton.click();
+        }
+
+        // step 5 Select random year
+        List<WebElement> years = getDriver().findElements(By.xpath("//div[contains(@class,'ant-picker-cell-inner')]"));
+        Random random = new Random();
+        WebElement randomYear = years.get(random.nextInt(years.size())); // Pick a random year
+        randomYear.click();
+
+        // step 6 select random month
+
+        List<WebElement> months = getDriver().findElements(By.xpath("//div[contains(@class,'ant-picker-cell-inner')]"));
+        WebElement randomMonth = months.get(random.nextInt(months.size()));
+        randomMonth.click();
+
+        // step 7 select random day from the choose month
+
+        List<WebElement> allDates = getDriver().findElements(By.xpath("//td[contains(@class, 'ant-picker-cell-in-view')]"));
+        WebElement randomDate = allDates.get(random.nextInt(allDates.size()));
+        Thread.sleep(2000);
+        randomDate.click();
+
+        // Edit passport number
+        studentPage.writeOneElement(studentPage.edit_passportNumber, "12345abcd");
+
+        // Edit issue date
+
+        studentPage.clickOneElement(studentPage.edit_issue_date);
+
+
+       // Find all available dates
+        List<WebElement> issueDates = getDriver().findElements(By.xpath("//td[contains(@class, 'ant-picker-cell-in-view')]/div"));
+        String targetIssueDate = "15";
+        for (WebElement dates: issueDates){
+//            System.out.println("Issue dates " +dates.getText());
+              if (dates.getText().trim().equals(targetIssueDate)){
+                  dates.click();
+                  break;
+              }
+        }
+
+        //Edit expire date
+
+        studentPage.clickOneElement(studentPage.edit_expire_date);
+
+        // Find all available dates
+        List<WebElement> expireDates = getDriver().findElements(By.xpath("//td[contains(@class, 'ant-picker-cell-in-view')]/div"));
+        String targetExpireDate = "30";
+        for (WebElement date: expireDates){
+//            System.out.println("Expire dates " + date.getText());
+            if (date.getText().trim().equals(targetExpireDate)){
+                date.click();
+                break;
+            }
+        }
+
+
+        // Edit gender
+
+        studentPage.clickOneElement(studentPage.edit_gender);
+        // Edit marital status
+
+        studentPage.clickOneElement(studentPage.edit_marital_status);
+        //Edit phone number
+        studentPage.getElement(studentPage.edit_phone_number).clear();
+        studentPage.writeOneElement(studentPage.edit_phone_number, "12445908656");
+        //Edit Email
+        studentPage.writeOneElement(studentPage.edit_email, "admin123@mail.com");
+
+        //Edit country of birth
+        studentPage.clickOneElement(studentPage.edit_country_birth);
+        Actions editCountryBirthAction = new Actions(getDriver());
+        editCountryBirthAction.sendKeys("Bangladesh").build().perform();
+        editCountryBirthAction.sendKeys(Keys.ENTER).build().perform();
+
+        //Edit country of residence
+        studentPage.clickOneElement(studentPage.edit_residence_country);
+        Actions editCountryResidenceAction = new Actions(getDriver());
+        editCountryResidenceAction.sendKeys("Bangladesh").build().perform();
+        editCountryResidenceAction.sendKeys(Keys.ENTER).build().perform();
+
+        //Edit country of nationality
+        studentPage.clickOneElement(studentPage.edit_country_nationality);
+        Actions editCountryNationalityAction = new Actions(getDriver());
+        editCountryNationalityAction.sendKeys("Bangladesh").build().perform();
+//        Thread.sleep(2000);
+        editCountryNationalityAction.sendKeys(Keys.ENTER).build().perform();
+
+        // save button
+        studentPage.clickOneElement(studentPage.save_and_next);
+        Thread.sleep(3000);
+//        WebDriverWait successWait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+//        successWait.until(ExpectedConditions.visibilityOfElementLocated(studentPage.student_info_updated_toast));
+
+        // back page
+        studentPage.clickOneElement(studentPage.go_to_stu_page);
+
+    }
+
     @Test
     public void DeleteAStudent() throws InterruptedException {
-       studentPage.LoadAnWebPage(studentPage.student_page_url);
-       studentPage.clickOneElement(studentPage.delete_student_btn_locator);
-       // wait until modal show the text
-       WebDriverWait confirmDeleteWait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
-       confirmDeleteWait.until(ExpectedConditions.textToBePresentInElementLocated(studentPage.confirm_delete_btn_text, "Do You Want To Delete This Student Information ?"));
-       studentPage.clickOneElement(studentPage.confirm_delete_student_btn_locator);
-       Thread.sleep(3000);
+        studentPage.LoadAnWebPage(studentPage.student_page_url);
+        studentPage.clickOneElement(studentPage.delete_student_btn_locator);
+        // wait until modal show the text
+        WebDriverWait confirmDeleteWait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+        confirmDeleteWait.until(ExpectedConditions.textToBePresentInElementLocated(studentPage.confirm_delete_btn_text, "Do You Want To Delete This Student Information ?"));
+        studentPage.clickOneElement(studentPage.confirm_delete_student_btn_locator);
+        Thread.sleep(3000);
     }
 
 }
